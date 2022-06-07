407270260781e6beb4d185816265bbe37d0eb200
hotsuitor <hotsuitor@qq.com>
2022-06-07 11:16:24 +0800

    chore: readd hop.nvim

--- plugins/init.lua
+++ plugins/init.lua
@@ -22,17 +22,16 @@ return {
       require "custom.plugins.configs.surround"
     end,
   },
-  -- 配置无效，暂时注释掉
-  -- ["phaazon/hop.nvim"] = {
-  --   cmd = {
-  --     "HopWord",
-  --     "HopLine",
-  --     "HopChar1"
-  --   },
-  --   config = function()
-  --     require "custom.plugins.configs.hop"
-  --   end,
-  -- },
+  ["phaazon/hop.nvim"] = {
+    cmd = {
+      "HopWord",
+      "HopLine",
+      "HopChar1"
+    },
+    config = function()
+      require "custom.plugins.configs.hop"
+    end,
+  },
   ["liuchengxu/vista.vim"] = {
     config = function()
       require "custom.plugins.configs.vista"
